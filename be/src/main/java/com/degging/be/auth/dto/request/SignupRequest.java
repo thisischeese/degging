476c7c0ba25498921188e8a51394ac7982652e63
K-DaHee <dhk086512@gmail.com>
2026-03-10 20:18:01 +0900

    feat(auth): 회원가입 로직에 성별 및 생년월일 반영 (S14P21A501-76)

--- be/src/main/java/com/degging/be/auth/dto/request/SignupRequest.java
+++ be/src/main/java/com/degging/be/auth/dto/request/SignupRequest.java
@@ -1,11 +1,12 @@
 package com.degging.be.auth.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import com.degging.be.user.entity.Gender;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 /**
  * 회원가입 요청 시 클라이언트로부터 전달받는 데이터 객체
@@ -27,4 +28,11 @@ public class SignupRequest {
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$", message = "비밀번호는 8~16자 영문, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
+    @NotNull(message = "성별은 필수 입력값입니다.")
+    private Gender gender;
+
+    @NotNull(message = "생년월일은 필수 입력값입니다.")
+    @Past(message = "생년월일은 과거 날짜여야 합니다.")
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    private LocalDate birthDate;
 }
