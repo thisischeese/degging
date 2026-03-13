f399e708a5a62f95da664df87d9e716c8136ed10
K-DaHee <dhk086512@gmail.com>
2026-03-13 15:46:45 +0900

    feat(cafe): 카카오 API 기반 카페 데이터 정교화 엔드포인트 구현 (S14P21A501-73)

--- be/src/main/java/com/degging/be/cafe/controller/CafeController.java
+++ be/src/main/java/com/degging/be/cafe/controller/CafeController.java
@@ -1,19 +1,21 @@
 package com.degging.be.cafe.controller;
 
 import com.degging.be.cafe.service.CafeCollectService;
+import com.degging.be.cafe.service.CafeDuplicateService;
 import com.degging.be.global.dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * 카페 데이터 수집 실행용 컨트롤러
+ * 카페 데이터 수집 및 저장을 담당하는 컨트롤러
  */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/cafes")
-public class CafeCollectController {
+public class CafeController {
 
     private final CafeCollectService cafeCollectService;
+    private final CafeDuplicateService cafeDuplicateService;
 
     /**
      * 카페 데이터 수집
@@ -21,8 +23,17 @@ public class CafeCollectController {
      * @return 데이터 수집 실행 성공 응답
      */
     @PostMapping("/collect")
-    public BaseResponse<Integer> collect() {
+    public BaseResponse<Void> collect() {
         cafeCollectService.collectCafes();
+        return BaseResponse.success();
+    }
+
+    /**
+     * 카카오 API 기반 카페 데이터 정교화
+     */
+    @PostMapping("/match")
+    public BaseResponse<Void> match() {
+        cafeDuplicateService.matchKakaoPlaces();
         return BaseResponse.success();
     }
 }
