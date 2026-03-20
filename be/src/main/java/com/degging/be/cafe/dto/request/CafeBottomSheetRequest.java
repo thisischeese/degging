9aa2af6641a17f89db616381b4d9f92e96213e2a
K-DaHee <dhk086512@gmail.com>
2026-03-20 10:17:36 +0900

    feat(map): 바텀시트 카페 리스트 조회 요청 DTO 추가 (S14P21A501-140)

---
+++ be/src/main/java/com/degging/be/cafe/dto/request/CafeBottomSheetRequest.java
@@ -0,0 +1,20 @@
+package com.degging.be.cafe.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 지도 바텀시트 카페 리스트 조회 요청 DTO
+ */
+@Getter
+@NoArgsConstructor
+public class CafeBottomSheetRequest {
+
+    private Double latitude;                    // 현재 사용자의 위도
+
+    private Double longitude;                   // 현재 사용자의 경도
+
+    private boolean includeFranchise = false;   // 프랜차이즈 포함 여부
+
+    private CafeBottomSheetSort sort = CafeBottomSheetSort.RECOMMEND;    // 정렬 기준
+}
