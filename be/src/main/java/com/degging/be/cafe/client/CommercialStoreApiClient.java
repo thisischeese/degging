1d9a425ce5e8893ca248573cc5d6ce28fe2389ac
K-DaHee <dhk086512@gmail.com>
2026-03-24 12:29:25 +0900

    feat(cafe): 상가정보 API 호출 시 업종 코드 파라미터화 (S14P21A501-188)

--- be/src/main/java/com/degging/be/cafe/client/CommercialStoreApiClient.java
+++ be/src/main/java/com/degging/be/cafe/client/CommercialStoreApiClient.java
@@ -16,9 +16,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 public class CommercialStoreApiClient {
 
-    // 카페 업종 코드 I21201 하드코딩
-    private static final String CAFE_SMALL_UPJONG_CODE = "I21201";
-
     // Builder가 아니라 이미 build된 WebClient를 직접 주입
     private final WebClient webClient;
 
@@ -26,13 +23,14 @@ public class CommercialStoreApiClient {
     private String serviceKey;
 
     /**
-     * 카페 업종(I21201)에 해당하는 상가업소 목록 조회
+     * 특정 업종 코드에 해당하는 상가업소 목록 조회
      *
+     * @param upjongCode 상권업종소분류코드 (ex. I21201)
      * @param pageNo 현재 페이지 번호
      * @param rows 페이지당 조회 건수
      * @return 업종별 상가업소 조회 응답 DTO
      */
-    public StoreListInUpjongResponse fetchCafeStores(int pageNo, int rows) {
+    public StoreListInUpjongResponse fetchCafeStores(String upjongCode, int pageNo, int rows) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .scheme("https")
@@ -42,7 +40,7 @@ public class CommercialStoreApiClient {
                         .queryParam("pageNo", pageNo)
                         .queryParam("numOfRows", rows)
                         .queryParam("divId", "indsSclsCd")
-                        .queryParam("key", CAFE_SMALL_UPJONG_CODE)
+                        .queryParam("key", upjongCode)
                         .queryParam("type", "json")
                         .build())
                 .accept(MediaType.APPLICATION_JSON)
