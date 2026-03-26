1def0f2c4a1d4df98785ff6f2a0a0a39a90da2b5
K-DaHee <dhk086512@gmail.com>
2026-03-26 13:26:18 +0900

    fix(config): JPA 플랫폼 트랜잭션 매니저 명시적 등록으로 No bean named 'transactionManager' 에러 해결 (S14P21A501-238)

--- be/src/main/java/com/degging/be/global/config/JpaConfig.java
+++ be/src/main/java/com/degging/be/global/config/JpaConfig.java
@@ -1,7 +1,12 @@
 package com.degging.be.global.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * Jpa 설정 파일
@@ -9,4 +14,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration
 @EnableJpaAuditing // 엔티티의 @CreatedDate, @LastModifiedDate 활성화
 public class JpaConfig {
+
+    @Primary
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
